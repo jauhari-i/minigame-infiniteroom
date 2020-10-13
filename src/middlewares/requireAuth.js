143b98bb91fef65e5fc25785136437c4013b0dbd
Irfan Jauhari <kangrykong123@gmail.com>
2020-10-14 01:17:56 +0700

    Fix middlewares auth

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -4,14 +4,13 @@ module.exports = checkToken = async (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization'];
   !token &&
     res.status(403).json({
-      msg: 'Login terlebih dahulu',
+      message: 'Login terlebih dahulu',
     });
   await jwt.verify(token.replace('Bearer ', ''), 'minigames-infiniteroom', (err, decoded) => {
     err &&
       res.status(401).json({
-        status: 401,
-        msg: 'Token tidak valid, silahkan login kembali',
-        err,
+        code: 401,
+        message: 'Token tidak valid, silahkan login kembali',
       });
     req.decoded = decoded;
     next();
